4c9337581b7b0be2ca7eca595cc9e14b88738573
torresal <alexander.p.torres@jol.nasa.gov>
2020-12-16 02:30:31 -0800

    added compile.sh for shadow mask updates

--- src/compile.sh
+++ src/compile.sh
@@ -1,4 +1,5 @@
 gcc -lm -o look_double look_double.c 
+gcc -lm -o look_msk look_msk.c 
 FFLAGS="-O2 -ffixed-line-length-132"
 gfortran $FFLAGS -o cpxlooks cpxlooks.f
 gfortran $FFLAGS -o rilooks rilooks.f
